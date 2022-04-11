07b1a4615bb7fcd0af78e6d6ef44ee31d0b93202
Ollie <33800208+wawz@users.noreply.github.com>
2022-04-11 11:17:35 +0800

    Add files via upload

---
+++ _next/static/Pr1Dlviw7zii7x06bDBL4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2ddd3cb1faf5eab7.css","static/chunks/pages/index-118943126a00fde9.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
